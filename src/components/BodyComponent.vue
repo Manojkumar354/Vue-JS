62b8c3a67d139a2db15125f9d432c5c75c7327d0
manoj <m.jaiswalk29@gmail.com>
2026-03-07 22:30:38 +0530

    make two new componentcommit

---
+++ src/components/BodyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>Component body</h2>
+</template>
+
+<script>
+export default {
+    name: 'BodyComponent',
+}
+</script>
+
+<style scoped>
+h2{
+    color: #4242b9;
+    text-align: center;
+}
+</style>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>Welcome to the Home Page</h1>
+</template>
+
+<script>
+export default {
+    name: 'HomeComponent',
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+    text-align: center;
+}
+
+</style>
