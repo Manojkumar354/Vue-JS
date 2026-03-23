8baa62a73f2b76df996bfa1ec1b0fed0162faf54
manoj <m.jaiswalk29@gmail.com>
2026-03-23 20:58:21 +0530

    Add new component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
+<h1>Simple Form</h1>
 
-<HomeComponents />
+<p>{{ form }}</p>
 
+<form>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" v-model="form.email"><br><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" v-model="form.password"><br><br>
+
+    <input type="submit" v-on:click="submitForm" value="Submit">
+</form>
 </template>
 
 <script>
-import HomeComponents from './components/HomeComponents.vue';
-
 export default {
     name: 'App',
-    components: {
-        HomeComponents       
+    data() {
+        return {
+            form: {
+                email: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        submitForm(event) {
+            event.preventDefault();
+            console.log(this.form);
+        }
     }
 }
 </script>
+
+<style scoped>
+  h1 {
+      color: orange;
+  }
+</style>
---
+++ src/components/RefComponent.vue
@@ -0,0 +1,29 @@
+<template>
+<h1>Ref in Vue 3</h1>
+
+<input type="text" ref="input" />
+
+<button @click="focusInput">Focus Input</button>
+
+</template>
+
+<script>
+export default {
+  name: "RefComponent",
+  methods: {
+    focusInput() {
+      this.$refs.input.focus();
+      let inputValue = this.$refs.input.value;
+      console.log(inputValue);
+      this.$refs.input.style.color = "red";
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+h1 {
+  color: orange;
+}
+</style>
