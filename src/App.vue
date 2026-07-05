c3d1b736a3d14a38edfb9a5478a67959af3d7f60
manoj <m.jaiswalk29@gmail.com>
2026-07-05 20:10:24 +0530

    add file of modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,48 @@
 <template>
 <h1>Simple Form</h1>
 
-<p>{{ form }}</p>
+<ul>
+    <li v-for="item in error" v-bind:key="item">
+        {{ item }}
+    </li>
+</ul>
 
 <form>
 
     <label for="email">Email:</label>
-    <input type="email" id="email" name="email" v-model="form.email"><br><br>
+    <input type="email" id="email" v-model="form.email"><br/><br/>
 
     <label for="password">Password:</label>
-    <input type="password" id="password" name="password" v-model="form.password"><br><br>
+    <input type="password" id="password" v-model="form.password"><br/><br/>
 
-    <label for="Country">Country</label>
-    <select name="country" id="country" v-model="form.country">
+    <label for="country">Country</label>
+    <select id="country" v-model="form.country">
         <option value="">Select a country</option>
         <option value="USA">USA</option>
         <option value="Canada">Canada</option>
         <option value="UK">UK</option>
-    </select><br><br>
+    </select>
 
     <h3>Technology</h3>
-    <label for="Java">Java</label>
-    <input type="checkbox" id="Java" name="technology" value="Java" v-model="form.technology">
-    <label for="Python">Python</label>
-    <input type="checkbox" id="Python" name="technology" value="Python" v-model="form.technology">
-    <label for="JavaScript">JavaScript</label>
-    <input type="checkbox" id="JavaScript" name="technology" value="JavaScript" v-model="form.technology"><br><br>
+    <label>
+        <input type="checkbox" value="Java" v-model="form.technology"> Java
+    </label>
+    <label>
+        <input type="checkbox" value="Python" v-model="form.technology"> Python
+    </label>
+    <label>
+        <input type="checkbox" value="JavaScript" v-model="form.technology"> JavaScript
+    </label>
 
     <h3>Gender</h3>
     <label for="male">Male</label>
-    <input type="radio" id="male" name="gender" value="male" v-model="form.gender">
+    <input type="radio" id="male" value="male" v-model="form.gender">
+
     <label for="female">Female</label>
-    <input type="radio" id="female" name="gender" value="female" v-model="form.gender">
+    <input type="radio" id="female" value="female" v-model="form.gender">
+
+    <br><br>
+    <button type="submit" @click.prevent="submitForm">Submit</button>
 </form>
 </template>
 
@@ -44,14 +55,24 @@ export default {
                 email: '',
                 password: '',
                 country: '',
-                technology: [], 
+                technology: [],
                 gender: ''
-            }
+            },
+            error: [],
         }
     },
     methods: {
-        submitForm(event) {
-            event.preventDefault();
+        submitForm() {
+            this.error = [];
+            for (const item in this.form) {
+                if (this.form[item] === '' || this.form[item].length === 0) {
+                    this.error.push(`${item} is required`);
+                }
+            }
+
+            if (this.error.length === 0) {
+                alert('Form submitted successfully!');
+            }
             console.log(this.form);
         }
     }
@@ -59,7 +80,7 @@ export default {
 </script>
 
 <style scoped>
-  h1 {
-      color: orange;
-  }
+h1 {
+    color: orange;
+}
 </style>
